6bcbc18c4cec5a3240dade09798ffb7cf8543c42
Matt Berteaux <mattyb678@gmail.com>
2015-12-06 07:55:59 -0700

    Make private functions

--- src/advent_of_code/day1.clj
+++ src/advent_of_code/day1.clj
@@ -3,15 +3,15 @@
 
 (def input-str (slurp (io/file (io/resource "not-quite-lisp.txt"))))
 
-(defn -count-occur [full-str paren]
+(defn- -count-occur [full-str paren]
   (count (re-seq (re-pattern (str "\\" paren)) full-str)))
 
-(defn -directions [dir-str]
+(defn- -directions [dir-str]
   (let [num-left (-count-occur dir-str "(")
         num-right (-count-occur dir-str ")")]
     (- (+ num-left 0) num-right)))
 
-(defn -position-at-basement
+(defn- -position-at-basement
   [pos coll floor]
   (if (< floor 0)
     pos
--- test/advent_of_code/day1_test.clj
+++ test/advent_of_code/day1_test.clj
@@ -1,47 +1,47 @@
 (ns advent-of-code.day1-test
   (:require [clojure.test :refer :all]
-            [advent-of-code.day1 :refer :all]))
+            [advent-of-code.day1 :as day1]))
 
 (deftest test1
   (testing "0"
-    (is (= 0 (-directions "(())")))))
+    (is (= 0 (#'day1/-directions "(())")))))
 
 (deftest test2
   (testing "0"
-    (is (= 0 (-directions "()()")))))
+    (is (= 0 (#'day1/-directions "()()")))))
 
 (deftest test3
   (testing "3"
-    (is (= 3 (-directions "(((")))))
+    (is (= 3 (#'day1/-directions "(((")))))
 
 (deftest test4
   (testing "3"
-    (is (= 3 (-directions "(()(()(")))))
+    (is (= 3 (#'day1/-directions "(()(()(")))))
 
 (deftest test5
   (testing "3"
-    (is (= 3 (-directions "))(((((")))))
+    (is (= 3 (#'day1/-directions "))(((((")))))
 
 (deftest test6
   (testing "-1"
-    (is (= -1 (-directions "())")))))
+    (is (= -1 (#'day1/-directions "())")))))
 
 (deftest test7
   (testing "-1"
-    (is (= -1 (-directions "))(")))))
+    (is (= -1 (#'day1/-directions "))(")))))
 
 (deftest test8
   (testing "-3"
-    (is (= -3 (-directions ")))")))))
+    (is (= -3 (#'day1/-directions ")))")))))
 
 (deftest test9
   (testing "-3"
-    (is (= -3 (-directions ")())())")))))
+    (is (= -3 (#'day1/-directions ")())())")))))
 
 (deftest test10
   (testing "pos 1"
-    (is (= 1 (-position-at-basement 0 ")" 0)))))
+    (is (= 1 (#'day1/-position-at-basement 0 ")" 0)))))
 
 (deftest test11
   (testing "pos 5"
-    (is (= 5 (-position-at-basement 0 "()())" 0)))))
+    (is (= 5 (#'day1/-position-at-basement 0 "()())" 0)))))
