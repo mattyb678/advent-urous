ee38f7d89a910b1ded78789e43b076ccc210352f
Matt Berteaux <mattyb678@gmail.com>
2015-12-24 21:34:10 -0700

    Finished with day 14 part2

--- src/advent_of_code/day14.clj
+++ src/advent_of_code/day14.clj
@@ -1,38 +1,54 @@
 (ns advent-of-code.day14
   (:require [clojure.java.io :as io]))
 
-(defn- -parse-line [s]
+(defn- -parse-reindeer [s]
   (let [regex #"^([\w]+).*\s(\d+)\skm\/s.*\s(\d+)\sseconds.*\s(\d+)\sseconds"
         matches (re-find regex s)]
     (if-not (nil? matches)
       {:name (nth matches 1)
        :speed (read-string (nth matches 2))
        :fly-time (read-string (nth matches 3))
-       :rest-time (read-string (nth matches 4))})))
+       :rest-time (read-string (nth matches 4))
+       :dist 0})))
 
 (defn- -dist-after [reindeer secs]
   (let [speed (reindeer :speed) fly-time (reindeer :fly-time)
-        rest-time (reindeer :rest-time)]
-    (loop [time-left secs dist 0 status :flying]
-      (cond
-       (>= 0 time-left) dist
-       (= status :flying)
-       (if (>= time-left fly-time)
-         (recur (- time-left fly-time) (+ dist (* fly-time speed)) :resting)
-         (recur (- time-left fly-time) (+ dist (* time-left speed)) :flying))
-       (= status :resting) (recur (- time-left rest-time) dist :flying)))))
+        rest-time (reindeer :rest-time)
+        num-flights (quot secs (+ fly-time rest-time))
+        left-over-time (- secs (* num-flights (+ rest-time fly-time)))
+        distance (+ (* speed fly-time num-flights)
+                    (* (min left-over-time fly-time) speed))]
+    (assoc reindeer :dist distance)))
+
+(defn- -first-place [reindeers]
+  ((apply max-key :dist reindeers) :dist))
+
+(defn- -reward-points [reindeers]
+  (let [leader-dist (-first-place reindeers)]
+    (map #(if (= leader-dist (% :dist))
+            (update-in % [:points] (fn [pts] (inc (or pts 0))))
+            %)
+         reindeers)))
 
 (defn- -input-lines [file-name]
   (with-open [rdr (io/reader (io/file (io/resource file-name)))]
     (doall (line-seq rdr))))
 
-(defn- -part-1 [lines time]
-  (->> lines
-       (map #(-parse-line %))
+(defn- -part-1 [reindeers time]
+  (->> reindeers
        (map #(-dist-after % time))
+       (map #(% :dist))
        (apply max)))
 
+(defn- -part-2 [parsed-reindeers time]
+  (loop [cur-time 1 reindeers parsed-reindeers]
+    (if (> cur-time time)
+      ((apply max-key :points (remove #(nil? (% :points)) reindeers)) :points)
+      (recur (inc cur-time)
+             (-reward-points (map #(-dist-after % cur-time) reindeers))))))
+
 (defn challenge []
-  (let [lines (-input-lines "day14.txt")]
+  (let [reindeers (map -parse-reindeer (-input-lines "day14.txt"))]
     (println "Day 14")
-    (println (-part-1 lines 2503))))
+    (println (-part-1 reindeers 2503))
+    (println (-part-2 reindeers 2503))))
--- test/advent_of_code/day14_test.clj
+++ test/advent_of_code/day14_test.clj
@@ -2,43 +2,54 @@
   (:require [clojure.test :refer :all]
             [advent-of-code.day14 :as day14]))
 
-(deftest test-parse-line1
-  (is (= {:name "Dancer" :speed 37 :fly-time 1 :rest-time 36}
-         (#'day14/-parse-line "Dancer can fly 37 km/s for 1 seconds, but then must rest for 36 seconds."))))
+(deftest test-parse-reindeer1
+  (is (= {:name "Dancer" :speed 37 :fly-time 1 :rest-time 36 :dist 0}
+         (#'day14/-parse-reindeer "Dancer can fly 37 km/s for 1 seconds, but then must rest for 36 seconds."))))
 
-(deftest test-parse-line2
-  (is (= {:name "Comet" :speed 14 :fly-time 10 :rest-time 127}
-         (#'day14/-parse-line "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."))))
+(deftest test-parse-reindeer2
+  (is (= {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 0}
+         (#'day14/-parse-reindeer "Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds."))))
 
-(deftest test-parse-line3
-  (is (= {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162}
-         (#'day14/-parse-line "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."))))
+(deftest test-parse-reindeer3
+  (is (= {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 0}
+         (#'day14/-parse-reindeer "Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds."))))
 
 (deftest test-dist-after1
-  (is (= 14
-         (#'day14/-dist-after {:name "Comet" :speed 14 :fly-time 10 :rest-time 127}
+  (is (= {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 14}
+         (#'day14/-dist-after {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 0}
                               1))))
 
 (deftest test-dist-after2
-  (is (= 16
-         (#'day14/-dist-after {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162}
+  (is (= {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 16}
+         (#'day14/-dist-after {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 0}
                               1))))
 
 (deftest test-dist-after3
-  (is (= 140
-         (#'day14/-dist-after {:name "Comet" :speed 14 :fly-time 10 :rest-time 127}
+  (is (= {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 140}
+         (#'day14/-dist-after {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 0}
                               10))))
 (deftest test-dist-after4
-  (is (= 160
-         (#'day14/-dist-after {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162}
+  (is (= {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 160}
+         (#'day14/-dist-after {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 0}
                               10))))
 
 (deftest test-dist-after5
-  (is (= 1120
-         (#'day14/-dist-after {:name "Comet" :speed 14 :fly-time 10 :rest-time 127}
+  (is (= {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 1120}
+         (#'day14/-dist-after {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 0}
                               1000))))
 
 (deftest test-dist-after6
-  (is (= 1056
-         (#'day14/-dist-after {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162}
+  (is (= {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 1056}
+         (#'day14/-dist-after {:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 0}
                               1000))))
+
+(deftest test-first-place1
+  (is (= 160
+         (#'day14/-first-place [{:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 160}
+                                {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 140}]))))
+
+(deftest test-part2-1
+  (is (= 1
+         (#'day14/-part-2 [{:name "Dancer" :speed 16 :fly-time 11 :rest-time 162 :dist 0}
+                           {:name "Comet" :speed 14 :fly-time 10 :rest-time 127 :dist 0}]
+                          1))))
